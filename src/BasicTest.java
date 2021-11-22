d35f4c5b45b01a5a6d7a934741e71d546eea6179
hkon123 <hkonhd@gmail.com>
2021-11-22 16:25:46 +0100

    added test case

---
+++ src/BasicTest.java
@@ -0,0 +1,42 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BasicTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		Packet testPacket = new Packet( 
+				"10.4.1.4",
+				6969,
+				10, //seq nr
+				100, //ackNr 
+				1,  // option = timeout
+				"");
+		testPacket.setRaw_data();
+		testPacket.setSequenceNr(0);
+		testPacket.stripPacket();
+		assertEquals(testPacket.getSequenceNr(), 10);
+	}
+
+}
